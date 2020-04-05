8394ea79fa66d4e419f0b0ab90cc34f28d6bda1a
Carl-W <carl.igelstroem@tiket.com>
2020-04-05 16:23:57 +0800

    updated the analytics ID

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppContainer from "./components/AppContainer";
 import LandingPage from "./components/Landing";
 import Auth from "./components/Auth";
 
+const history = createBrowserHistory();
 // Replace with your Google Property ID
-const trackingId = "UA-227193315";
+const trackingId = "G-CSDE3BW3C3";
+// Initialize google analytics page view tracking
 ReactGA.initialize(trackingId);
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <AppContainer>
         <Switch>
           <Route path='/auth'>
