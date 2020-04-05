557973445fbd3bbe1f6012c2cb0e430de03cc60b
Carl-W <carl.igelstroem@tiket.com>
2020-04-05 16:29:09 +0800

    removed React Analytics things, and added it the old fashioned way.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
-import ReactGA from "react-ga";
-import { createBrowserHistory } from "history";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppContainer from "./components/AppContainer";
 import LandingPage from "./components/Landing";
 import Auth from "./components/Auth";
 
-const history = createBrowserHistory();
-// Replace with your Google Property ID
-const trackingId = "G-CSDE3BW3C3";
-// Initialize google analytics page view tracking
-ReactGA.initialize(trackingId);
-
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
-
 function App() {
   return (
-    <Router history={history}>
+    <Router>
       <AppContainer>
         <Switch>
           <Route path='/auth'>
