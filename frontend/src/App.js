0bab55788cc308d35adf2c2efd65fb368e116fec
Carl-W <carl.igelstroem@tiket.com>
2020-04-05 16:08:52 +0800

    Added google analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ReactGA from "react-ga";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppContainer from "./components/AppContainer";
 import LandingPage from "./components/Landing";
 import Auth from "./components/Auth";
+
+// Replace with your Google Property ID
+const trackingId = "UA-227193315";
+ReactGA.initialize(trackingId);
 
 function App() {
   return (
