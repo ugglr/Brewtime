1b84db89bee4b2f17b10ba07a17b1c738d4c9ece
Carl-W <carl.igelstroem@tiket.com>
2020-07-01 20:52:20 +0800

    MongoDB connects correctly.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,44 @@
+require('dotenv').config();
 // Express
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 // Apollo / GQL
-const { ApolloServer, gql } = require("apollo-server-express");
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
+const { ApolloServer, gql } = require('apollo-server-express');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
+const startServer = async () => {
+  console.log('[SERVER] Starting express server...');
+  const app = express();
 
-// Apply Middlewares
-server.applyMiddleware({ app });
+  console.log('[SERVER] Starting apollo server...');
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+  // Apply Middlewares
+  console.log('[SERVER] Applying middleware...');
+  server.applyMiddleware({ app });
+
+  // Connect MongoDB with mongoose
+  console.log('[SERVER] Connecting to MongoDB...');
+
+  mongoose
+    .connect(`${process.env.MONGO_CONNECTION_STRING}`, {
+      useNewUrlParser: true,
+    })
+    .then(() =>
+      app.listen({ port: 4000 }, () =>
+        console.log(
+          `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+        )
+      )
+    )
+    .catch((mongoConnectionError) => {
+      console.log(
+        '[SERVER] We have a problem connecting to MongoDB...',
+        mongoConnectionError
+      );
+      throw new Error(mongoConnectionError);
+    });
+};
+
+startServer();
